ca6a3a13ce62e21cbea90aa511cecd220794f5ed
King009 <132940375+KINGGGGGA@users.noreply.github.com>
2024-03-24 12:50:03 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://apkts.xyz/graphql"
+	const endpoint = "https:www.google.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://en.technomedia.in/how-to-save-money-on-car-insurance/?utm_source=viki&utm_medium=RAS98&utm_campaign=RAS98`
+					`www.google.com`
 				}`,
 			},
 		};
